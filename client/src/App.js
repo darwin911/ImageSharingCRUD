7b1307df6809c277778152369e74a179c30bbd46
aefritz <austin.fritz33@gmail.com>
2019-03-16 13:09:30 -0400

    added libraries cloudinary, react file base 64, created sample path for upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
-let api_key = PROCESS.env.REACT_APP_API_KEY;
-let api_secret = PROCESS.env.REACT_APP_API_SECRET;
+import {Cloudinary} from 'cloudinary-react';
+import FilesBase64 from 'react-file-base64';
+import uploadPhoto from './services/services';
+import UploadFile from './components/UploadFile'
 
-const cloudinaryCore = new cloudinary.Cloudinary({cloud_name: 'photo-sharing-app'});
+let api_key = process.env.REACT_APP_API_KEY;
+let api_secret = process.env.REACT_APP_API_SECRET;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filepath: ''
+    }
+    this.handleUpload = this.handleUpload.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  async handleUpload (ev) {
+    ev.preventDefault();
+    let {filepath} = this.state
+    let resp = await uploadPhoto(filepath.base64);
+    return resp;
+  }
+
+  handleChange (ev) {
+    const {name, value} = ev.target;
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  getFiles(filepath) {
+    this.setState({
+      filepath: filepath
+    })
+  }
+
   render() {
     return (
-      <CloudinaryContext cloudName="photo-sharing-app" apiKey={api_key} apiSecret={api_secret}>
       <div className="App">
-
+          <CloudinaryContext cloudName="photo-sharing-app" apiKey={api_key} apiSecret={api_secret}>
+            <h2>This is an image retrieved from our Cloudinary account through the React SDK.</h2>
+            <Image publicId="sample" width="300" />
+            <form>
+              <FilesBase64
+              multiple={ false }
+              onDone={ this.getFiles.bind(this) } />
+              <button type='submit' onClick={this.handleUpload}>upload</button>
+            </form>
+          </CloudinaryContext>
       </div>
-      </CloudinaryContext>
     );
   }
 }
