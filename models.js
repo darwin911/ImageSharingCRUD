fc42dfb0e6d274d269533fdad8747a1673226104
aefritz <austin.fritz33@gmail.com>
2019-03-18 13:10:54 -0400

    match database name to heroku

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
-  database: 'postPicle',
+  database: process.env.DATABASE_URL,
   dialect: 'postgres',
   operatorsAliases: false,
   define: {
