c4500e3efb83f70585c35bbe0cca693260d1e96e
MikeyF53 <mikefang530@gmail.com>
2019-03-18 15:17:17 -0400

    adds register

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-const Register = () => {
+const Register = (props) => {
+  const { name, email, password, handleChange, handleSubmit } = props;
   return (
-    <div>
-      <p>Register</p>
-    </div>
-  )
-}
+    <form onSubmit={props.handleSubmit}>
+      <h3>Register Form</h3>
+      <label>Name</label>
+      <input
+        type='text'
+        name='name'
+        id='name'
+        onChange={handleChange}
+        value={name}
+      />
+      <label>Email</label>
+      <input
+        type='text'
+        name='email'
+        id='email'
+        onChange={handleChange}
+        value={email}
+      />
+      <label>Password</label>
+      <input
+        type='text'
+        name='password'
+        id='password'
+        onChange={handleChange}
+        value={password}
+      />
+      <button onClick={handleSubmit} type='submit'>
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Register;
