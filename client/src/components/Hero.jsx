d4965a65bf571f52f522a817a7b6c1549a08c18a
Akinyemi Ogundipe <ijnuthomefolder@ool-ad02062f.dyn.optonline.net>
2019-03-19 19:07:03 -0400

    Welcome message in hero component and commented out image example

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Hero = () => {
   return (
     <div>
-      <p>Hero</p>
+        <h1 className="title"><span>Post</span>Pic</h1>
+      <div className="hero">
+      {/*<img src="https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2019/lightning.jpg" />*/}
+      <p>Welcome to PostPic, where you can post a pic!</p>
+      </div>
     </div>
   )
 }
