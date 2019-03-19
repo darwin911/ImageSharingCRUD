ec7c3acd16d36e1623703f0f06a4989b7808be28
MikeyF53 <mikefang530@gmail.com>
2019-03-19 11:55:42 -0400

    start on reel and post

--- client/src/components/Reel.jsx
+++ client/src/components/Reel.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Reel = () => {
+const Reel = props => {
   return (
     <div>
+      {props.reelPost.map(post => (
+        <div>
+          <p>Title Bar</p>
+          {/* {post.title}  */}
+          <p>Description area</p>
+          {/* {post.description} */}
+        </div>
+      ))}
       <p>Reel</p>
     </div>
-  )
-}
+  );
+};
 
 export default Reel;
