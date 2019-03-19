dc238af47e1b3ec3e12120d27654876dacb2f2a2
MikeyF53 <mikefang530@gmail.com>
2019-03-18 23:28:41 -0400

    adds postform

--- client/src/components/PostForm.jsx
+++ client/src/components/PostForm.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const PostForm = () => {
+const PostForm = props => {
+  const { title, description, publicId, handleChange, handleSubmit } = props;
   return (
-    <div>
-      <p>PostForm</p>
-    </div>
-  )
-}
-
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        name='title'
+        placeholder='Title'
+        onChange={handleChange}
+        value={title}
+      />
+      <input
+        type='text'
+        name='description'
+        placeholder='Description...'
+        onChange={handleChange}
+        value={description}
+      />
+      <input type='text' name='publicId' value={publicId} />
+      <button onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+};
 export default PostForm;
