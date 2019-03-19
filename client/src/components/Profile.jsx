e6c2131d62c6b17df234ea9f60c62c8868b65770
MikeyF53 <mikefang530@gmail.com>
2019-03-19 14:22:21 -0400

    adds profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Profile = props => {
   return (
-    
     <div className='profile'>
-      <img src='' alt='' />
-      <p>name:</p>
-      <p>bio:</p>
+      {props.currentUser.map(user => (
+        <div>
+          <img src='' alt='' />
+          <p>{user.name}</p>
+          <p>{user.bio}</p>
+          {/* Edit profile buton */}
+          <button onClick={props.handleSubmit}>Edit</button>
+        </div>
+      ))}
     </div>
   );
 };
