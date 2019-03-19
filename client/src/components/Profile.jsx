1cf454563d8236206f6b502e3ed49b067991c2ba
Darwin Smith <darwinspsmith@gmail.com>
2019-03-19 18:59:13 -0400

    style for Profile Section and Edit Button, and PostForm Submit button

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const Profile = props => {
+  const proPicStyle = {
+    background: "url(" + props.currentUser.pro_pic + ")",
+    height: '100px',
+    width: '100px',
+    backgroundSize: 'cover',
+    borderRadius: '50%',
+  }
   return (
     <div className='profile'>
       {props.currentUser &&
         <>
-          <img className="pro-pic" src={props.currentUser.pro_pic} alt='props.currentUser.pro_pic' />
+          <div
+            className="pro-pic"
+            style={proPicStyle}
+            alt='' />
           <p><strong>{props.currentUser.name}</strong></p>
           <p>{props.currentUser.bio}</p>
           <button>Edit</button>
