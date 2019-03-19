27c74086b90e0e229ae666a7f8fa8a37f1968ba3
aefritz <austin.fritz33@gmail.com>
2019-03-18 23:25:20 -0400

    add restrict middleware, add paths for update posts and delete post, left space for Akineymi, add samples to scratch.js

--- auth.js
+++ auth.js
@@ -7,10 +7,22 @@ const hash = async (pass) => await bcrypt.hash(pass, 10);
 const compare = async (pass, hashedPass) => await bcrypt.compare(pass, hashedPass);
 const encode = async (data) => await jwt.sign(data, SECRET);
 const verify = async (token) => await jwt.verify(token, SECRET);
+const restrict = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const data = jwt.verify(token, SECRET);
+    res.locals.user = data; //do we need this line?
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(403).send('Unauthorized');
+  }
+}
 
 module.exports = {
   hash,
   compare,
   encode,
   verify,
+  restrict
 };
