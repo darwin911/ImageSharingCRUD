c96b345a67fca83d44341124b2d83bc67058a1a1
aefritz <austin.fritz33@gmail.com>
2019-03-20 13:11:47 -0400

    delete works

--- client/src/components/Reel.jsx
+++ client/src/components/Reel.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Post from './Post';
 
 const Reel = props => {
-  let {updateReel, handleDelete} = props;
-  console.log(props.reelPosts.map(post => post.title))
+  let {updateReel, handleDelete, currentUser, reelPosts} = props;
+  //console.log(props.reelPosts.map(post => post.title))
+  //console.log(props.reelPosts.map(post => post.id))
+  //console.log(currentUser)
   return (
     <main>
       {props.reelPosts && props.reelPosts.map(post => (
@@ -12,7 +14,8 @@ const Reel = props => {
           postId={post.id}
           title={post.title}
           description={post.description}
-          handleDelete={handleDelete}/>
+          handleDelete={handleDelete}
+          currentUser={currentUser}/>
       ))}
     </main>
   );
