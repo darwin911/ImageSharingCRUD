e3be1144ec064a26c2ab808164a43b87677d77f6
aefritz <austin.fritz33@gmail.com>
2019-03-21 22:39:46 -0400

    testing new server routes

---
+++ client/build/precache-manifest.4c4664f3d26d6d8b75b271594cb9836b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bc1e0de63ac90128ca5",
+    "url": "/static/css/main.a395c982.chunk.css"
+  },
+  {
+    "revision": "0bc1e0de63ac90128ca5",
+    "url": "/static/js/main.243c266c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca7eaece848214387b3d",
+    "url": "/static/js/2.b120892e.chunk.js"
+  },
+  {
+    "revision": "a2006699afb014db5d6d4f8e6f1b623c",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d0f43f64c06eb06ec50d08dcfaf4e94.js"
+  "/precache-manifest.4c4664f3d26d6d8b75b271594cb9836b.js"
 );
 
 workbox.clientsClaim();
